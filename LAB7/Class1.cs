9fd12eafe1d040c440e1e81ca764607f1b2c5717
grabsasha66@gmail.com <grabsasha66@gmail.com>
2026-03-11 20:24:25 +0200

    2.2

--- LAB7/Class1.cs
+++ LAB7/Class1.cs
@@ -2,7 +2,7 @@
 
 class BankTerminal
 {
-    public Action<int> OnMoneyWithdraw;
+    public event Action<int> OnMoneyWithdraw;
 
     public void Withdraw(int amount)
     {
@@ -16,11 +16,13 @@ class Program
     static void Main()
     {
         BankTerminal terminal = new BankTerminal();
-
         terminal.OnMoneyWithdraw += (amount) => Console.WriteLine($"Успішно знято: {amount} грн");
 
-        terminal.OnMoneyWithdraw = null;
+        // Наступні рядки тепер викликатимуть ПОМИЛКИ КОМПІЛЯЦІЇ:
+        // terminal.OnMoneyWithdraw = null; 
+        // Помилка CS0070: Подія може з'являтися лише в лівій частині += або -= (за винятком випадків використання всередині типу BankTerminal)
 
-        terminal.OnMoneyWithdraw?.Invoke(1000000);
+        // terminal.OnMoneyWithdraw?.Invoke(1000000); 
+        // Помилка CS0070: Подія не може бути викликана безпосередньо ззовні класу.
     }
 }
